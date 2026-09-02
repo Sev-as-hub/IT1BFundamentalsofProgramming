5a54e8cfceff8621eee2afb0e34fe8445d76b0ad
jerome <valdezcedrc@gmail.com>
2026-09-02 20:55:29 +0800

    Assignment 3 ko sir

---
+++ Assignment3.java/src/Sevilla.java
@@ -0,0 +1,20 @@
+public class Sevilla { // Declares a class named UserInformation
+
+    public static void main(String[] args) {
+        String firstName = "Cedric Chirs Jerome";
+        String lastName = "Sevilla";
+        String address = "San Antonio, Santo Tomas, Pangasinan";
+        int mobileNumber = 287400813;
+        int age = 18;
+        double gwa = 99.50;
+
+        System.out.println("Firstname: " + firstName);
+        System.out.println("Lastname: " + lastName);
+        System.out.println("Address: " + address);
+        System.out.println("Mobile Number: 09" + mobileNumber);
+        System.out.println("Age: " + age);
+        System.out.println("GWA: " + gwa);
+
+    }
+
+}
